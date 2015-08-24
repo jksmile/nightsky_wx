a62db62f16cad34cbd73974c9a5b41eeab63c708
jinkai.xu <xjksmile@gmail.com>
2015-08-24 22:44:59 +0800

    update

--- src/main/java/com/xjk/project/controller/TestController.java
+++ src/main/java/com/xjk/project/controller/TestController.java
@@ -1,13 +1,7 @@
 package com.xjk.project.controller;
 
-import com.xjk.project.service.Performer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * Intro:
